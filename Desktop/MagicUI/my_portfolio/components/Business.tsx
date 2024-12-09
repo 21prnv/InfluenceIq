036951a94d0cc725cfc98f62703b5798cfde63f9
Karan kendre <karankendreg@gmail.com>
2024-12-09 13:25:27 +0530

    Responsive Errors

--- Desktop/MagicUI/my_portfolio/components/Business.tsx
+++ Desktop/MagicUI/my_portfolio/components/Business.tsx
@@ -6,7 +6,7 @@ const Business = () => {
   return (
     <div 
     id='freelancing'
-    className=' lg:m-h-screen py-20 md:py-4  h-full flex flex-col items-center justify-center xl:px-32'>
+    className=' lg:m-h-screen py-20  md:py-4  h-full flex flex-col items-center justify-center xl:px-32'>
         <h2
           className="text-4xl px-10 sm:py-16 lg:text-7xl font-bold  text-center bg-gradient-to-r from-[#f1f5f966] via-[#f1f5f9] via-51% to-[#f1f5f966] text-transparent bg-clip-text"
         >
