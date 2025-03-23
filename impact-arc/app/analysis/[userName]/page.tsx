4a27a0f25e79cac15b0b8e9bb3e5bcb5eafc5742
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2025-03-24 00:20:53 +0530

    deployed changes

--- impact-arc/app/analysis/[userName]/page.tsx
+++ impact-arc/app/analysis/[userName]/page.tsx
@@ -117,7 +117,7 @@ export default function DashboardPage() {
     setError(null);
 
     try {
-      const scrapeResponse = await fetch("/api/scrap", {
+      const scrapeResponse = await fetch(`${process.env.BACKEND_URL || 'http://localhost:5000'}/api/scrape-instagram`,  {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username: inputUsername }),
